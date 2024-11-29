caed2a7e7b76213937b3c9283ec2df25d4f03d0f
RonaldoNL <rnl77016@udelasalle.edu.mx>
2024-11-29 08:17:13 -0600

    Database configuration

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 app = FastAPI()
+
+# Configuración de la base de datos SQLite
+DATABASE_URL = "sqlite:///./movies.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
