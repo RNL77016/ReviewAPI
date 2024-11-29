b4974c14d0fe814b54dd86701558de3c014b5150
RonaldoNL <rnl77016@udelasalle.edu.mx>
2024-11-29 08:41:06 -0600

    Database Models created

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 app = FastAPI()
 
@@ -9,4 +9,31 @@ DATABASE_URL = "sqlite:///./movies.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Modelos de base de datos
+class User(Base):
+    _tablename_ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+class Movie(Base):
+    _tablename_ = "movies"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, unique=True, index=True)
+    description = Column(String)
+    year = Column(Integer)
+    genre = Column(String)
+    rating = Column(Float)
+    image_url = Column(String, nullable=True)
+
+    reviews = relationship("Review", back_populates="movie")
+
+class Review(Base):
+    _tablename_ = "reviews"
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+    rating = Column(Float)
+    movie_id = Column(Integer, ForeignKey("movies.id"))
+    movie = relationship("Movie", back_populates="reviews")
 
